611f69f8bb80cd0c04fe237f72e8a242d074d86e
Jack Donnelly <jackdonnelly02@gmail.com>
2025-03-19 16:46:00 +0000

    allows total user input

--- Create_Dataset_Ecom.py
+++ Create_Dataset_Ecom.py
@@ -1,30 +1,65 @@
 import pandas as pd
 import numpy as np
 import random
+from datetime import datetime, timedelta
 
-numcust = 100
+num_columns = int(input("Enter amount of columns: "))
+columns = []
 
-cust_ids =[f"C{101 + i}" for i in range(numcust)]
+for i in range(num_columns):
+    column_name = input(f"Enter name of column {i+1}:")
+    columns.append(column_name)
 
-purchase_amount_range = (0.0, 300.0)
-new_item_range = (0.0, 0.0)
+boundaries = {}
+col_types = {}
 
-productA = [round(random.uniform(*purchase_amount_range), 2) for _ in range(numcust)]
-productB = [round(random.uniform(*purchase_amount_range), 2) for _ in range(numcust)]
-productC = [round(random.uniform(*purchase_amount_range), 2) for _ in range(numcust)]
-productD = [round(random.uniform(*purchase_amount_range), 2) for _ in range(numcust)]
-ProductE = [round(random.uniform(*purchase_amount_range), 2) for _ in range(numcust)]
-new_item = [np.nan for _ in range(numcust)]
+for column in columns:
+    col_type = input(f"Enter type of column '{column}', int, float, str or date: ")
+    col_types[column] = col_type
 
-df = pd.DataFrame ({
-    "Customer ID": cust_ids, 
-    "Product A": productA, 
-    "Product B": productB, 
-    "Product C": productC, 
-    "Product D": productD, 
-    "Product E": ProductE,
-    "New Product": new_item
-})
+    if col_type in ['int', 'float']:
+        min_val = input(f"Enter min value for column '{column}': ")
+        max_val = input(f"Enter max value for column '{column}': ")
+        boundaries[column] = (min_val, max_val)
+    elif col_type == 'str':
+        values = input(f"Enter strings for column '{column}' (comma seperated): ").split(',')
+        boundaries[column] = values
+    elif col_type == 'date':
+        date = input(f"Enter date for column '{column}' (YYYY-MM-DD): ")
+        date = datetime.strptime(date, "%Y-%m-%d")
+        boundaries[column] = date
 
-df.to_csv('purchase_data.csv', index = False)
+numentries = 10000
+data = {}
+prev_col_val = {}
+
+for i, column in enumerate(columns):
+    col_type = col_types[column]
+
+    if col_type == 'int':
+        min_val, max_val = boundaries[column]
+        data[column] = np.random.randint(int(min_val), int(max_val) +1, size = numentries)
+    elif col_type == 'float':
+        min_val, max_val = boundaries[column]
+        data[column] = np.random.uniform(float(min_val), float(max_val), size = numentries)
+    elif col_type == 'str':
+        values = boundaries[column]
+        data[column] = [np.random.choice(values) for _ in range(numentries)]
+    elif col_type == 'date':
+        date = boundaries[column]
+
+        d_days = 365
+        date_column_val = []
+        for index in range(numentries):
+            if i > 0 and data[columns[i-1]][index] == 0:
+                date_column_val.append('N/A')
+            else :
+                rand_date = date + timedelta(days = np.random.randint(0, d_days))
+                date_column_val.append(rand_date)
+            data[column] = date_column_val
+
+df = pd.DataFrame(data)
+
+filename = input("Enter name for this file, e.g example.csv: ")
+df.to_csv(filename, index = False)
 print(df.head())
